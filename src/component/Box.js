6787699425c8c11ddce522f0d8593e235eb15f92
sunae shin <ko1607@naver.com>
2024-03-21 01:31:25 +0900

    first commit

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Box = (props) =>{
+    // console.log("넘어오니?", props)
+console.log(props.userBoxColor, props.computerBoxColor)
 
-const Box = (props) => {
-    console.log("넘어오니?", props)
+const userBoxStyle = {
+    width: '200px',
+    height: '600px',
+    borderRadius: '500px',
+    backgroundColor: `${props.userBoxColor}`,
+    color: 'white'
+};
+
+const computerBoxStyle = {
+    width: '200px',
+    height: '600px',
+    borderRadius: '500px',
+    backgroundColor: `${props.computerBoxColor}`,
+    color: 'white'
     
+};
+
     return (
         
-        <div >
+        <div className='center'>
            <h1 className='title'>{props.title}</h1>
-           <div className='Box'>
-           <img className='img' src={props.item && props.item.img} art='주먹'></img>
+           <img className='img' src={props.item && props.item.img} art='주먹'></img>          
            {/* 명확하게 정해지지 않는 값일경우 앞에 가드 넣어줘야 해 (props.item && ) */}
-            </div>
-            <div className='rebox'>
-            <h1>결과</h1>
-            </div>
+            <p className={`boxColor${props.computerBoxColor}`}></p>
+            <p className={`boxColor${props.userBoxColor}`}></p>
+            <h1 className='title'>{props.userResult}</h1>
+            <h1 className='title'>{props.computerResult}</h1>
+            
         </div>
     );
 };
